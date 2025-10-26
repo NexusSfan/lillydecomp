f2efdb8cce9a5cd35f66fa85cd85aa3762e457e8
NexusSfan <nexussfan@duck.com>
2025-10-26 07:25:34 -0700

    Fix doccheck

--- tools/doccheck.py
+++ tools/doccheck.py
@@ -6,22 +6,23 @@ file = sys.argv[1]
 with open(file, encoding="utf-8") as f:
     lines = f.readlines()
 
-all_funcs = ["".join(line.split()) for line in lines if re.search(r"^Lf[a-z0-9]{3}$", line)]
+all_funcs_undoc = ["".join(line.split()) for line in lines if re.search(r"^Lf[a-z0-9]{3}$", line)]
 
 TOTAL_FUNCS = 215
 
-print(f"{len(all_funcs)}/{TOTAL_FUNCS} functions documented")
+all_funcs = TOTAL_FUNCS - len(all_funcs_undoc)
 
-all_vars = ["".join(line.split()) for line in lines if line.startswith("ram_")]
+print(f"{all_funcs}/{TOTAL_FUNCS} functions documented")
+
+all_vars_undoc = ["".join(line.split()) for line in lines if line.startswith("ram_")]
 
 TOTAL_VARS = 90
 
-print(f"{len(all_vars)}/{TOTAL_VARS} variables documented")
+all_vars = TOTAL_VARS - len(all_vars_undoc)
 
-func_percent_undoc = int(str(len(all_funcs)/TOTAL_FUNCS)[2:4])
-var_percent_undoc = int(str(len(all_vars)/TOTAL_VARS)[2:4])
+print(f"{all_vars}/{TOTAL_VARS} variables documented")
 
-func_percent = 100 - func_percent_undoc
-var_percent = 100 - var_percent_undoc
+func_percent = int(str(all_funcs/TOTAL_FUNCS)[2:4])
+var_percent = int(str(all_vars/TOTAL_VARS)[2:4])
 
 print(f"{func_percent}% of functions are documented. {var_percent}% of variables are documented.")
