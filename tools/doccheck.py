3f64021cdce4d3821dbbc9c7eb35e21095a20dfb
NexusSfan <nexussfan@duck.com>
2025-11-26 11:50:58 -0800

    Fix `doccheck`

--- tools/doccheck.py
+++ tools/doccheck.py
@@ -24,7 +24,20 @@ all_vars = TOTAL_VARS - len(all_vars_undoc)
 
 print(f"{all_vars}/{TOTAL_VARS} variables documented")
 
-func_percent = int(str(all_funcs/TOTAL_FUNCS)[2:4])
-var_percent = int(str(all_vars/TOTAL_VARS)[2:4])
+func_percent_raw = str(all_funcs/TOTAL_FUNCS)
+if len(func_percent_raw) == 3:
+    func_percent_raw = f"{func_percent_raw}0"
+
+var_percent_raw = str(all_vars/TOTAL_VARS)
+if len(var_percent_raw) == 3:
+    var_percent_raw = f"{var_percent_raw}0"
+
+func_percent = int(func_percent_raw[2:4])
+var_percent = int(var_percent_raw[2:4])
 
 print(f"{func_percent}% of functions are documented. {var_percent}% of variables are documented.")
+
+total_progress_raw = int(int(func_percent) + int(var_percent)) / 200
+total_progress = int(str(total_progress_raw)[2:4])
+
+print(f"Total progress: {total_progress}%")
